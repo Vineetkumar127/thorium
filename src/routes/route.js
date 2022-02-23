3d8c2cd2cd48f32eceb60391ea1c42f9daff0dbd
Vineet kumar <vk3144634@gmail.com>
2022-02-23 20:08:36 +0530

    code logger

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const logger = require("../logger/logger")
 
 router.get('/test-me', function (req, res) {
+    logger.printWelcomeMessage()
     res.send('My first ever api!')
 });
 
+router.get('/test-me1', function (req, res) {
+    res.send('My first API')
+});
 module.exports = router;
