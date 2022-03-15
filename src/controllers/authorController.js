ec0255ea2082b24749b3b8657217437d632c432b
Vineet kumar <vk3144634@gmail.com>
2022-03-15 10:47:47 +0530

    project_blog

---
+++ src/controllers/authorController.js
@@ -0,0 +1,18 @@
+const authorModel = require("../models/authorModel")
+
+
+
+
+const createAuthor = async function (req, res) {
+    try{
+        var data = req.body
+    let savedData = await authorModel.create(data)
+    res.status(200).send({ data: savedData })
+}catch(error){
+    res.status(500).send({status:"failed",message:error.message})
+    }
+
+}
+
+
+module.exports.createAuthor = createAuthor
---
+++ src/routes/route.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+//--------------------------------------------------
+const authorController =require('../controllers/authorController')
+const blogController = require ('../controllers/blogController')
+
+//****************************************** */
+
+router.post("/Authors" ,authorController.createAuthor )
+
+router.post('/blogs', blogController.Blogs)
+
+router.get('/getBlog', blogController.getBlogs)
+
+router.put('/blogs/:blogId', blogController.updating)
+
+//router.delete('/blogs/:blogId', blogController.deleting)
+
+
+
+
+
+
+
+module.exports = router;
